e4ab45699a7fd9e7886c58df63aa0ff0f1ce201d
Eric Humpert <ethumpert@gmail.com>
2016-05-13 10:21:18 -0500

    completed following the examples of the tutorial on Canvas and turned all the examples into comments

--- canvas.js
+++ canvas.js
@@ -2,3 +2,20 @@ var canvas = document.getElementById('exampleCanvas'),
     context = canvas.getContext('2d');
 
     // code examples will continue here...
+
+    /* canvas line example
+    context.beginPath();
+    context.moveTo(50, 50);
+    context.lineTo(250,150);
+    context.stroke();
+    */
+
+    /* rectangle example
+    context.fillStyle = 'yellow';
+    context.fillRect(50, 50, 200, 100);
+    */
+
+    /* drawing text example
+    context.font = 'italic 40pt Calibri, sans-serif';
+    context.fillText('Hello World!', 50, 50);
+    */
